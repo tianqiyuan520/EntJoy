90e6a18eb9eab34c86b6d6f4156cc3697a4fb4ba
tianqiyuan520 <1540131499@qq.com>
2026-05-31 23:38:42 +0800

    调整 .gitignore

--- src/Godot/.godot/mono/temp/obj/ExportRelease/Godot.AssemblyInfo.cs
+++ src/Godot/.godot/mono/temp/obj/ExportRelease/Godot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Godot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportRelease")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a56419d56521d773cb9f7a118b0514ae4f0b42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0b271b3f1f34d8ed8eb43d28b8e6a532bfd0a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Godot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Godot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
